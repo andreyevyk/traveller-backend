8c52b49c4425e4caad4fa5b281d3ce1552dba00b
Andrey <andreyevyk13@gmail.com>
2022-02-06 21:57:11 -0300

    feat: authenticate user

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,51 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+    if (!user) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const passwordMatch = await compare(password, user.password);
+    if (!passwordMatch) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const token = sign({}, "5f4a869487ca69b8f67796d01430e60a", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      token,
+    };
+  }
+}
+export { AuthenticateUserUseCase };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { citiesRoutes } from "./cities.routes";
 import { userRoutes } from "./users.routes";
 
@@ -7,5 +8,6 @@ const router = Router();
 
 router.use("/cities", citiesRoutes);
 router.use("/users", userRoutes);
+router.use(authenticateRoutes);
 
 export { router };
