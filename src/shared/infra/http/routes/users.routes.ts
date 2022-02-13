995b54262d581f2a11ee13035ac92958164f542a
Andrey <andreyevyk13@gmail.com>
2022-02-13 20:42:46 -0300

    feat: remove ensureAuthenticated create user

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -2,12 +2,10 @@ import { Router } from "express";
 
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 
-import { ensureAuthenticated } from "../middlewares/ensureAuthenticate";
-
 const userRoutes = Router();
 
 const createUserController = new CreateUserController();
 
-userRoutes.post("/", ensureAuthenticated, createUserController.handle);
+userRoutes.post("/", createUserController.handle);
 
 export { userRoutes };
