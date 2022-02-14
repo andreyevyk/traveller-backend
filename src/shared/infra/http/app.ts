294d45a45e35b8de11211736113fe4e22d9e98f2
Andrey <andreyevyk13@gmail.com>
2022-02-14 00:15:13 -0300

    fix: yarn install

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -23,9 +23,9 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/files", express.static(uploadConfig.uploadsFolder));
 
+app.use(cors());
 app.use(router);
 
-app.use(cors());
 app.use(
   (err: Error, request: Request, response: Response, next: NextFunction) => {
     if (err instanceof AppError) {
